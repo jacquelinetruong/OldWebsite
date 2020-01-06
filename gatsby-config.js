a3abea430d9557f137e3ec50ad60b15fbb804ac9
++jt <itsjacquellinetruong@gmail.com>
2020-01-05 22:58:26 -0500

    Adding my first website

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,19 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata:{
+    title: 'Jacqueline Truong',
+    author: 'Jacqueline Truong',
+  },
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark'
+  ]
 }
